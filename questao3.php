a21c8722b9958981570f9d018c0210ec39c2e027
Davi Antônio Rodrigues Leal <120655510+DaviLeal87@users.noreply.github.com>
2023-08-05 22:53:13 -0300

    Adicionando questão 3, pergunta nº 1

---
+++ questao3.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="stylequestao3.css">
+    <title>Questão 3</title>
+</head>
+<body>
+    <header>
+        <h2>Trabalho webdev</h2>
+    </header>
+        
+        <h3>Questão 3 - Questionário com resposta em formato de links</h2>
+      
+        <h4>Q1.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat iure voluptatum alias fuga. Perspiciatis, officia?</h4>
+
+            <p><a href="questao3b.php?valor1=1" class="alternativa">a&#41 Lorem ipsum dolor sit amet adipisicing elit.</a></p>
+            <p><a href="questao3b.php?valor1=2" class="alternativa correta">b&#41Lorem ipsum dolor sit amet consectetur adipisicing elit.</a></p>
+            <p><a href="questao3b.php?valor1=3" class="alternativa">c&#41Lorem ipsum dolor amet consectetur adipisicing elit.</a></p>
+            <p><a href="questao3b.php?valor1=4" class="alternativa">d&#41Lorem ipsum dolor sit amet consectetur adipisicing elit.</a></p>
+            <p><a href="questao3b.php?valor1=5" class="alternativa">e&#41Lorem ipsum dolor sit amet consectetur adipisicing.</a></p>
+
+        <p><a href="questao3.php">Cancelar</a></p>
+        <p><a href="index.html">Página Inicial</a></p>
+    <footer>
+    &copy; 2023 Davi Rodrigues & João Braga
+    </footer>
+</body>
+</html>
